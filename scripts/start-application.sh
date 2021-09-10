7aa5ecf25ab80d27c041b2180220e9edb719fd78
Rehan Ali <rehannali@ntpu.info>
2021-09-10 21:14:00 +0500

    Add flag if you want to access localhost of host machine from container itself.

--- scripts/start-application.sh
+++ scripts/start-application.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/eureka-server
 docker build -t rehannali/eureka-server .
-docker run --name eureka-server -d -p 8086:8086 rehannali/eureka-server
+docker run --name eureka-server --add-host host.docker.internal:host-gateway -d -p 8086:8086 rehannali/eureka-server
 docker logs -f eureka-server > /home/ec2-user/logs/eureka-server.log &
